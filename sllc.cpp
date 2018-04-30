e6a0556a88902f1fe3e6c6c4d854bdb740f804ef
1717051047-Fikri Aulia <38536861+fikriaulia23@users.noreply.github.com>
2018-05-01 01:16:36 +0700

    Update sllc.cpp

--- sllc.cpp
+++ sllc.cpp
@@ -7,37 +7,32 @@
 #include <windows.h>
 using namespace std;
 
-class node 
-{
+class node{
   public:					
   int angka;
   node *next; 				
 }; 
 
-
 node *awal_node = NULL;	
 
-void tambahdepan(int parameter)
-{
+void tambahdepan(int parameter){
   node *baru;			
   baru = new node;		
   cout << "Masukkan Angka   : ";
   cin >> baru->angka;   
-  baru->next = NULL;	
-  if(awal_node == NULL)	
-	  {
+  baru->next = NULL;
+	
+  if(awal_node == NULL){
 	    awal_node=baru;		
 	    awal_node->next = NULL;
-	  }
-  else
-	  {
+  }
+  	else{
 	    baru->next = awal_node;
 	    awal_node = baru;
-	  }
+	}
 } 
 
-void tambahbelakang(int parameter)
-  {
+void tambahbelakang(int parameter){
   node *temp, *temp2, *posisi;   
   temp = new node; 
    
@@ -46,205 +41,177 @@ void tambahbelakang(int parameter)
     
   temp->next = NULL; 
 									
-	  if (awal_node == NULL)
-		  {
-		    awal_node = temp;
-		    posisi = awal_node;
-		  }
-	  else
-		  {
-		    temp2 = awal_node;
-		    										
-		    while (temp2->next != NULL)		
-			    {
-			      temp2 = temp2->next; 			 
-			    }
+	  if (awal_node == NULL){
+		  awal_node = temp;
+		  posisi = awal_node;
+	  }
+	  	else{
+			temp2 = awal_node;
+			while (temp2->next != NULL){
+				temp2 = temp2->next;
+			}
 		  temp2->next = temp;
-		  }
+		}
 } 
 
-void print_node()
-{
+void print_node(){
   node *temp;
   temp = awal_node;
   cout << endl;
+	
 	  if (temp == NULL)
 	    	cout << "List kosong!" << endl;
-	  else
-		  {
-		    while (temp != NULL)
-			    {
-			     cout <<temp->angka << ", ";
-			     temp = temp->next; 
-			    }
-		    cout<<endl;
-		  }
+	  else{
+		while (temp != NULL){
+			cout <<temp->angka << ", ";
+			temp = temp->next;
+		}
+    cout<<endl;
+	  }
 } 
 
-void hapusdepan(int parameter)
-{
+void hapusdepan(int parameter){
   node *temp;
   temp = awal_node;
   awal_node = awal_node->next;
   delete temp;
 } 
 
-void hapusbelakang(int parameter)
-{
+void hapusbelakang(int parameter){
   node *temp1, *temp2;
 	  if (awal_node == NULL)
-	    cout << "List kosong!" << endl;
-	  else
-	  {
-	    temp1 = awal_node;
-		    if (temp1->next == NULL)
-			    {
+		  cout << "List kosong!" << endl;
+	  	else{
+	    		temp1 = awal_node;
+		   	if (temp1->next == NULL){
 			      delete temp1;
 			      awal_node = NULL;
-			    }
-		    else 
-			    {
-			      while (temp1->next != NULL)
-				      {
-				        temp2 = temp1;
-				        temp1 = temp1->next;
-				      }
+			}
+		    		else {
+			      		while (temp1->next != NULL){
+				        	temp2 = temp1;
+				        	temp1 = temp1->next;
+				      	}
 			      delete temp1;
 			      temp2->next = NULL;
-			    }
+			    	}
 	
-	   }
+		}
 } 
 
-void tambahtengah(int parameter)
-{
+void tambahtengah(int parameter){
   node *baru, *bantu;
   int posisi_sisip;
-	  if(awal_node != NULL)
-	  {
+	
+	  if(awal_node != NULL){
 	    cout<<"Akan disisip setelah Data Ke ? : ";
 	    cin>>posisi_sisip;
 	    bantu=awal_node;
 	    baru =new node;
-		    for(int i=1;i<posisi_sisip;i++)
-				{
-			    	if(bantu->next != NULL)
-			    		bantu=bantu->next;
-
-			      	else
-			      		continue;
-			    }
+		  
+		    for(int i=1;i<posisi_sisip;i++){
+			    if(bantu->next != NULL)
+			    bantu=bantu->next;
+		      	else
+				continue;
+		    }
+		  
 		  cout << "Masukkan Angka     : ";
 		  cin >> baru->angka;
 		  baru->next=bantu->next;
 		  bantu->next=baru;
 	  }
-	  else
-	  {
-	    cout<<"Belum ada data !!";
-	    system("pause");
-	  }
+	  	else{
+	    		cout<<"Belum ada data !!";
+	    		system("pause");
+	  	}
 }
 
-void hapustengah(int parameter)
-{
+void hapustengah(int parameter){
   int banyakdata,posisi_hapus,poshapus;
   node *hapus, *bantu;
-	  if(awal_node != NULL)
-		{
+	  if(awal_node != NULL){
 		    cout<<"Akan dihapus pada data ke : ";
 		    cin>>posisi_hapus;
 		    banyakdata=1;
 		    bantu=awal_node;
-			    while(bantu->next != NULL)
-				    {
-				      bantu=bantu->next;
-				      banyakdata++;
-				    }
-		    if((posisi_hapus<1)||(posisi_hapus>banyakdata))
-		    {
-		      cout<<"Belum ada data !! \n";
+			    while(bantu->next != NULL){
+				    bantu=bantu->next;
+				    banyakdata++;
+			    }
+		    if((posisi_hapus<1)||(posisi_hapus>banyakdata)){
+			    cout<<"Belum ada data !! \n";
 		    }
-		    else
-			    {
-			      bantu=awal_node;
-			      poshapus=1;
-				      while(poshapus<(posisi_hapus-1))
-					      {
-					        bantu=bantu->next;
+		    	else{
+				bantu=awal_node;
+				poshapus=1;
+					while(poshapus<(posisi_hapus-1)){
+						bantu=bantu->next;
 					        poshapus++;
-					      }
+					}
 			      hapus=bantu->next;
 			      bantu->next=hapus->next;
 			      delete hapus;
-			    }
-		}
- 	else
-    cout<<"Data Masih kosong, tidak bisa hapus data dari tengah! ";
+			}
+	  }
+ 		else
+			cout<<"Data Masih kosong, tidak bisa hapus data dari tengah! ";
 }
 
-void banyakangka()
-{
+void banyakangka(){
 	cout<<"Masukkan banyak data :";
 } 
 
-int main()
-{
+int main(){
 	int n,dat; 
   	awal_node = NULL;
     
     cout << "# Menambah List Node Depan." << endl;
     banyakangka();
     cin >> n;
-	    for (int i = 0; i < n; i++)
-	    {
-			tambahdepan(dat);
-		}
-		print_node();
+	    for (int i = 0; i < n; i++){
+		    tambahdepan(dat);
+	    }
+	print_node();
 		
 	cout << "\n# Menambah List Node Belakang." << endl;
 	banyakangka();
 	cin >> n;
-	    for (int i = 0; i < n; i++)
-	    {
-			tambahbelakang(dat);
-		}
-		print_node();
+	    for (int i = 0; i < n; i++){
+		    tambahbelakang(dat);
+	    }
+	print_node();
 		
-    cout << "\n# Menyisipkan List Node di posisi tertentu."<< endl;
-    banyakangka();
+	cout << "\n# Menyisipkan List Node di posisi tertentu."<< endl;
+	banyakangka();
 	cin >> n;
-	    for (int i = 0; i < n; i++)
-	    {
-			tambahtengah(dat);
-		}
-		print_node();
+	    for (int i = 0; i < n; i++){
+		    tambahtengah(dat);
+	    }
+	print_node();
 	
-    cout << "\n# Hapus List Node Depan." << endl;
-    banyakangka();
+	cout << "\n# Hapus List Node Depan." << endl;
+	banyakangka();
 	cin >> n;
-	    for (int i = 0; i < n; i++)
-	    {
-			hapusdepan(dat);
-		}
-		print_node();
+	    for (int i = 0; i < n; i++){
+		    hapusdepan(dat);
+	    }
+	print_node();
 	
-    cout << "\n# Hapus List Node Belakang." << endl;
-    banyakangka();
+	cout << "\n# Hapus List Node Belakang." << endl;
+	banyakangka();
 	cin >> n;
-	    for (int i = 0; i < n; i++)
-	    {
-			hapusbelakang(dat);
-		}
-		print_node();
+	    for (int i = 0; i < n; i++){
+		    hapusbelakang(dat);
+	    }
+	print_node();
 	
-    cout << "\n# Hapus List Node tertentu." << endl;
+	cout << "\n# Hapus List Node tertentu." << endl;
   	banyakangka();
 	cin >> n;
-	    for (int i = 0; i < n; i++)
-	    {
-			hapustengah(dat);
-			print_node();
-		}
+	    for (int i = 0; i < n; i++){
+		    hapustengah(dat);
+		    print_node();
+	    }
     
 }
